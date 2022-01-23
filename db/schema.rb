b025f98693af1c4a29c634159f26feffc8c5d8b7
TakammotoYurie <happyyuupy@gmail.com>
2022-01-23 21:28:40 +0900

    add: rails db:migrateコマンドの実行

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,17 @@
 
 ActiveRecord::Schema.define(version: 2022_01_22_073102) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
+    t.string "name", null: false
     t.string "crypted_password"
     t.string "salt"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
 
 end
