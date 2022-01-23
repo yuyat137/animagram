ac9613955e7dce7bf8bd5f2f2c28b996b3e4279e
TakammotoYurie <happyyuupy@gmail.com>
2022-01-23 21:06:18 +0900

    delete: 不要なファイルの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,6 +56,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:email, :crypted_password, :salt)
+      params.require(:user).permit(:email, :password, :password_confirmation)
     end
 end
