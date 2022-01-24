16dc081af4471beb18455d796199a8acef8b7ee6
TakammotoYurie <happyyuupy@gmail.com>
2022-01-25 05:09:19 +0900

    add: ログイン、ログアウトのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: %i[new create]
   root 'static_pages#top'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
