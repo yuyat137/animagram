2231abc6098b8b78126db0dc6921b29663133e44
TakammotoYurie <happyyuupy@gmail.com>
2022-01-26 15:17:20 +0900

    add: validationの追加とuserとのassociationを追加

---
+++ app/models/article.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  title       :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :bigint
+#
+# Indexes
+#
+#  index_articles_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+class Article < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :description, length: { maximum: 300 }
+
+  belongs_to: user
+end
