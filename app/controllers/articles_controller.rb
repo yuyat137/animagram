7446357136e4018bc8efd52deefbe535d8cb0917
TakammotoYurie <happyyuupy@gmail.com>
2022-01-27 03:14:31 +0900

    add: articleの一覧表示をするためのコードを記述

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
-  def new
+  skip_before_action :require_login, only: %i[index]
+
+  def index
+    @articles = Article.all.includes(:user).order(created_at: :desc)
   end
 end
