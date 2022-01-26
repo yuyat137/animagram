9a664576509b89a1361d44bb49cee9147f358ee1
TakammotoYurie <happyyuupy@gmail.com>
2022-01-26 19:01:03 +0900

    add: rails g controller Articlesの実行

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
+# == Route Map
+#
+
 Rails.application.routes.draw do
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create destroy]
+  resources :articles
   root 'static_pages#top'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
