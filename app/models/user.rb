a07e3dbd7e33df021a2503995dfe81a295847508
TakammotoYurie <happyyuupy@gmail.com>
2022-01-28 01:52:37 +0900

    fix: コードの記述が足りていない部分があったため追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,9 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
   validates :email, uniqueness: true, presence: true
-  validates :password, presence: true
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
   has_many :articles, dependent: :destroy
 end
