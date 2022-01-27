80f7067fd354a2bf6a5c27d1fbea53573713220e
TakammotoYurie <happyyuupy@gmail.com>
2022-01-28 03:35:08 +0900

    add: ModelSpec-validationのテストを記述

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -21,16 +21,52 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   describe 'validation' do
-    it 'is valid with all attributes' do
-      article = build(:article)
-      expect(article).to be_valid
-      expect(article.errors).to be_empty
+    context 'すべての属性が有効な場合' do
+      it '有効であること' do
+        article = build(:article)
+        expect(article).to be_valid
+        expect(article.errors).to be_empty
+      end
     end
 
-    it 'is invalid without title' do
-      article_without_title = build(:article, title: "")
-      expect(article_without_title).to be_invalid
-      expect(article_without_title.errors[:title]).to_eq ["can't be blank"]
+    context 'titleが存在しない場合' do
+      it '無効であること' do
+        article_without_title = build(:article, title: '')
+        expect(article_without_title).to be_invalid
+        expect(article_without_title.errors[:title]).to eq ["can't be blank"]
+      end
+    end
+
+    context 'titleが50字以下の場合' do
+      it '有効であること' do
+        article = build(:article, title: 'a' * 50)
+        expect(article).to be_valid
+        expect(article.errors).to be_empty
+      end
+    end
+
+    context 'titleが51文字以上の場合' do
+      it '無効であること' do
+        article_over_title = build(:article, title: 'a' * 51 )
+        expect(article_over_title).to be_invalid
+        expect(article_over_title.errors[:title])
+      end
+    end
+
+    context 'descriptionが300文字以下の場合' do
+      it '有効であること' do
+        article = build(:article, description: 'a' * 300 )
+        expect(article).to be_valid
+        expect(article.errors).to be_empty
+      end
+    end
+
+    context 'descriptionが301文字以上の場合' do
+      it '無効であること' do
+        article = build(:article, description: 'a' * 301 )
+        expect(article).to be_invalid
+        expect(article.errors[:description])
+      end
     end
   end
 end
