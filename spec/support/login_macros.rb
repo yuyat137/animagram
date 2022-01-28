8b055225a6b79f11f3b8b5b56a435081cf514e57
TakammotoYurie <happyyuupy@gmail.com>
2022-01-29 00:47:55 +0900

    fix: 目的が重複しているコードがあったため削除

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -2,7 +2,7 @@ module LoginMacros
   def login_as(user)
     visit root_path
     click_link 'Login'
-    fill_in 'email', with:user.email
+    fill_in 'email', with: user.email
     fill_in 'password', with: 'password'
     click_button 'Login'
   end
