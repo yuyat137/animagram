bbcb30b67a5268c7d1bfe5c6a4319b584d1d8fcc
TakammotoYurie <happyyuupy@gmail.com>
2022-01-29 00:08:33 +0900

    fix: 誤っている箇所があったためコードの軽微な修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,7 +6,7 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path, notice: 'ユーザー登録が完了しました'
+      redirect_back_or_to root_path, notice: 'ログインしました'
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -1,9 +1,9 @@
 module LoginMacros
   def login_as(user)
     visit root_path
-    click_button 'Login'
+    click_link 'Login'
     fill_in 'email', with:user.email
-    fill_in 'passsword', with:user.password
+    fill_in 'password', with: 'password'
     click_button 'Login'
   end
 end
