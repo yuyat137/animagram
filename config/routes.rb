a1d1c145308c074a184e89bac70d4125106a1274
TakammotoYurie <happyyuupy@gmail.com>
2022-01-31 15:42:51 +0900

    add: commentsのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@
 Rails.application.routes.draw do
   resources :users, only: %i[new create destroy]
   resources :articles
+    resources :comments, only: %i[create, destroy], shallow: true
   root 'static_pages#top'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
