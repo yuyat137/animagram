618b9d9be2619e322ccfad61a43cb979bf778033
TakammotoYurie <happyyuupy@gmail.com>
2022-02-03 01:41:06 +0900

    add: 画像形式やサイズのvalidationの追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -24,4 +24,33 @@ class Article < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 50 }
   validates :description, length: { maximum: 300 }
+  validates :images, presence: true
+  validate :image_type, :image_size, :image_length
+
+  private
+
+  def image_type
+    images.each do |image|
+      if !image.blob.content_type.in?(%('image/jpeg image/png'))
+        image.purge
+        image.add(:images, 'はjpegまたはpng形式でアップロードしてください')
+      end
+    end
+  end
+
+  def image_size
+    images.each do |image|
+      if image.blob.byte_size > 5.megabytes
+        image.purge
+        errors.add(:images, "は1つのファイル5MB以内にしてください")
+      end
+    end
+  end
+
+  def image_length
+    if images.length > 5
+      images.purge
+      errors.add(:images, "は5枚以内にしてください")
+    end
+  end
 end
