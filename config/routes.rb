b73a9723a9bbe60d495b4d137bcde7b1c4ba6473
TakammotoYurie <happyyuupy@gmail.com>
2022-02-13 19:50:18 +0900

    update: トップ画面をarticlesの一覧に変更

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments, only: %i[create destroy], shallow: true
   end
-  root 'static_pages#top'
+  root 'articles#index'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
