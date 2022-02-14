dddbe3a5ec5a8ca3d11b2b50b930f5261897abc5
TakammotoYurie <happyyuupy@gmail.com>
2022-02-15 02:18:13 +0900

    add: favoritesのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@
 #
 
 Rails.application.routes.draw do
+  get 'favorites/create'
+  get 'favorites/destroy'
   resources :users, only: %i[new create destroy]
   resources :articles do
     resources :comments, only: %i[create destroy], shallow: true
   end
+  resources :favorites, only: %i[index create destroy]
+
   root 'articles#index'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
