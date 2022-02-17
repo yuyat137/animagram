bf4124131e736c3bf6db5a7760dfe5ef10fbd396
TakammotoYurie <happyyuupy@gmail.com>
2022-02-17 15:51:18 +0900

    add: favorite_articlesのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,9 @@ Rails.application.routes.draw do
   resources :users, only: %i[new create destroy]
   resources :articles do
     resources :comments, only: %i[create destroy], shallow: true
-    collection do
-      get :favorites
-    end
     resource :favorite, only: %i[create destroy]
   end
+  resources :favorite_articles, only: :index
 
   root 'articles#index'
   get 'login', to: 'user_sessions#new'
