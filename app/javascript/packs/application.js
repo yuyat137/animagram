8be0cfb6348a03faf88cc4c035000fa73f11efe7
TakammotoYurie <happyyuupy@gmail.com>
2022-02-21 22:14:42 +0900

    delete: ActiveStorageの設定記述を削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,11 +5,11 @@
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
+// import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '../css/application.css'
 
 Rails.start()
 Turbolinks.start()
-ActiveStorage.start()
+// ActiveStorage.start()
 require("jquery")
