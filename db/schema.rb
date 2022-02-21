8acb047d11a0b20debe378ed8a0f7b70b341691b
TakammotoYurie <happyyuupy@gmail.com>
2022-02-21 22:36:29 +0900

    add: fog-aws(gem)のbundle installの実行と.gitignoreへの記述追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_14_023124) do
+ActiveRecord::Schema.define(version: 2022_02_21_132221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,15 @@ ActiveRecord::Schema.define(version: 2022_02_14_023124) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
+    t.string "image"
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "comments", force: :cascade do |t|
