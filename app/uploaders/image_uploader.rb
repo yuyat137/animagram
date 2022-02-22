91cd790d7c0b855e0295ff45d9935541c08071bf
TakammotoYurie <happyyuupy@gmail.com>
2022-02-23 03:23:32 +0900

    update: credentialsを使う形にコードを変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,9 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-  if Rails.env.development?
-    storage :fog
-  elsif Rails.env.test?
+  if Rails.env.test?
     storage :file
   else
     storage :fog
