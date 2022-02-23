ed79043d29a9d95fcdd517cb89f7990ae06bf161
TakammotoYurie <happyyuupy@gmail.com>
2022-02-23 15:13:42 +0900

    update: 不要な記述を削除

--- spec/system/articles_spec.rb
+++ spec/system/articles_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'article', type: :system do
     context 'ログインしていない場合' do
       it 'article作成リンクが表示されていないこと' do
         visit articles_path
-        expect(page).not_to have_link('写真を投稿する'), '未ログイン時に写真を投稿するリンクが表示されています'
+        expect(page).not_to have_link('写真を投稿する')
       end
     end
 
@@ -16,14 +16,14 @@ RSpec.describe 'article', type: :system do
       it 'article作成リンクが表示されていること' do
         login_as(user)
         visit articles_path
-        expect(page).to have_link('写真を投稿する'), '未ログイン時に写真を投稿するリンクが表示されています'
+        expect(page).to have_link('写真を投稿する')
       end
     end
 
     context '掲示板が一件もない場合' do
       it '何もない旨が表示されていること' do
         visit articles_path
-        expect(page).to have_content('写真がありません'), '掲示板が一件もない場合、「写真がありません」というメッセージが表示されていません'
+        expect(page).to have_content('写真がありません')
       end
     end
 
@@ -31,9 +31,9 @@ RSpec.describe 'article', type: :system do
       it 'article一覧が表示されていること' do
         article
         visit articles_path
-        expect(page).to have_content(article.title), 'articleのtitleが表示されていません'
-        expect(page).to have_content(article.description), 'articleのdescriptionが表示されていません'
-        expect(page).to have_content(article.user.name), 'articleを作成したuserの名前が表示されていません'
+        expect(page).to have_content(article.title)
+        expect(page).to have_content(article.description)
+        expect(page).to have_content(article.user.name)
         expect(page).to have_selector("img[src$='something.jpg']")
       end
     end
@@ -43,7 +43,7 @@ RSpec.describe 'article', type: :system do
     context 'ログインしていない場合' do
       it 'ログインページにリダイレクトされること' do
         visit edit_article_path(article)
-        expect(current_path).to eq('/login'), 'ログインページにリダイレクトされていません'
+        expect(current_path).to eq('/login')
         have_content 'ログインしてください'
       end
     end
@@ -56,9 +56,9 @@ RSpec.describe 'article', type: :system do
       it 'articleの詳細が表示されること' do
         visit articles_path
         click_on article.title
-        expect(page).to have_content(article.title), 'articleのtitleが表示されていません'
-        expect(page).to have_content(article.description), 'articleのdescriptionが表示されていません'
-        expect(page).to have_content(article.user.name), 'articleを作成したuserの名前が表示されていません'
+        expect(page).to have_content(article.title)
+        expect(page).to have_content(article.description)
+        expect(page).to have_content(article.user.name)
         expect(page).to have_selector("img[src$='something.jpg']")
       end
     end
@@ -68,7 +68,7 @@ RSpec.describe 'article', type: :system do
     context 'ログインしていない場合' do
       it 'ログインページにリダイレクトされること' do
         visit new_article_path
-        expect(current_path).to eq('/login'), 'ログインページにリダイレクトされていません'
+        expect(current_path).to eq('/login')
         have_content 'ログインしてください'
       end
     end
