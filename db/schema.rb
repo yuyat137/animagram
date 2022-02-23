567f1cd7b9d74d6c8fff818fb91cacd0c1a81886
TakammotoYurie <happyyuupy@gmail.com>
2022-02-23 22:00:04 +0900

    add: rails g model Category name:stringとrails db:migrateの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_151009) do
+ActiveRecord::Schema.define(version: 2022_02_23_125743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2022_02_21_151009) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "comments", force: :cascade do |t|
