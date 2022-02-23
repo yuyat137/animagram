6fff576e52cba505b69eb275d02c7cb8ef8de4f2
TakammotoYurie <happyyuupy@gmail.com>
2022-02-23 22:20:08 +0900

    add: テーブル作成時に自動で変更

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -8,6 +8,7 @@
 #  title       :string           not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  category_id :integer
 #  user_id     :bigint
 #
 # Indexes
