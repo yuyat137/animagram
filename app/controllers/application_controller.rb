b432fb620b72d69250db02c943c3612b8adb243b
TakammotoYurie <happyyuupy@gmail.com>
2022-03-09 17:50:44 +0900

    update: ファイル名を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,22 +3,22 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  rescue_from ActionController::RoutingError, with: :render_404
-  rescue_from Exception, with: :render_500
+  rescue_from ActiveRecord::RecordNotFound, with: :render404
+  rescue_from ActionController::RoutingError, with: :render404
+  rescue_from Exception, with: :render500
 
-  def render_404(exception = nil)
+  def render404(exception = nil)
     if exception
       logger.info "Rendering 404 with exception: #{exception.message}"
     end
-    render template: "errors/error_404", status: 404, layout: 'application'
+    render template: 'errors/error404', status: :not_found, layout: 'application'
   end
 
-  def render_500(exception = nil)
+  def render500(exception = nil)
     if exception
       logger.info "Rendering 500 with exception: #{exception.message}"
     end
-    render template: "errors/error_500", status: 500, layout: 'application'
+    render template: 'errors/error500', status: :internal_server_error, layout: 'application'
   end
 
   def require_login
